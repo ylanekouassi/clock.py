404cfc7522ed9a62390fa3aa7410ea85594567ba
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-03-13 23:20:59 -0400

    clock's styled

--- clock.py
+++ clock.py
@@ -7,6 +7,10 @@ window.title("Digital Clock")
 
 def clock():
     screen = strftime("%H:%M:%S %p")
+    label.config(text=screen)
+    label.after(1000, clock)
+
+label = Label(window, font=("digital-1",50, "bold"), background="black", foreground="green")
 
 
 window.mainloop()
